b1ad9ff6b05060d9b13830d92e31f57fd5f74f6b
jztein <jiayuaw@yahoo.com.sg>
2013-10-30 23:58:28 -0700

    init heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forest::Application.config.secret_token = '5b2f6cdc8e7d9e937d5b11ae541392d40fa10620030cf1b912716e8f471f377ded41bb019fa1029aff04bda19acbbe72bf35547b169780eba137e3dbdeda5bc0'
