b9cf5605451ebe22ba4a029d03817379ac502b5d
jztein <jiayuaw@yahoo.com.sg>
2013-11-07 00:15:26 -0800

    reverting back to before fb_graph

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Forest::Application.routes.draw do
   get "home/index"
   root to: "home#index"
 
+  match "hangouts/index" => "hangouts#index", :via => 'get'
+  match "hangouts/new" => "hangouts#new", :via => 'get'
+  match "hangouts/edit" => "hangouts#edit", :via => 'get'
+  match "hangouts/show" => "hangouts#show", :via => 'get'
+
   resources :hangouts
 end
